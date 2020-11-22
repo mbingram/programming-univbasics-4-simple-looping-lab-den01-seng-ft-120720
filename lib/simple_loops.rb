1abdf87d8282a468a8478978476c8a4c6c8dc6a5
charming-input-2976 <mbingramm@gmail.com>
2020-11-22 20:34:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,13 @@
-# Write your methods here
+require 'pry'
+
+def loop_message_five_times(message)
+  counter = 0
+  
+  binding.pry
+  
+  while counter <= 5 do
+    puts message[counter]
+    counter += 1
+  end
+end
+
