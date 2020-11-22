bdab14c4cc7ddfe347d2484a1b773d8cb94840d3
charming-input-2976 <mbingramm@gmail.com>
2020-11-22 20:40:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,4 +8,11 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times(message, n)
+def loop_message_n_times(message, 5)
+  counter = 0
+  
+  while counter =< 5 do
+    puts message
+    counter += 1 
+  end
+end
